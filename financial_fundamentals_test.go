1491a855ebc9a2b94cff7368e0196aadcfc4f036
Fatih Emin Öge <fatih@finfree.co>
2025-01-14 17:12:16 +0300

    feat: add GetStockStatsV2

--- financial_fundamentals_test.go
+++ financial_fundamentals_test.go
@@ -71,6 +71,32 @@ func (s *FinancialFundamentalsTestSuite) TestGetStockStats() {
 	require.NotEqual(s.T(), currentStockStats.FiveYear, 0.0)
 }
 
+func (s *FinancialFundamentalsTestSuite) TestGetStockStatsV2() {
+	client := newTestClient(s.Config)
+
+	ctx := context.Background()
+
+	resp, err := client.GetStockStatsV2(ctx, []string{"TUPRS"}, RegionTr)
+	require.NoError(s.T(), err)
+	require.NotEmpty(s.T(), resp)
+	currentStockStats := resp[0]
+	require.NotEmpty(s.T(), currentStockStats)
+	require.Equal(s.T(), "TUPRS", currentStockStats.Symbol)
+	require.Greater(s.T(), currentStockStats.PreviousClose, 0.0)
+	require.Greater(s.T(), currentStockStats.MarketCap, 0.0)
+	require.NotEqual(s.T(), currentStockStats.PeRatio, 0.0)
+	require.NotEqual(s.T(), currentStockStats.PbRatio, 0.0)
+	require.Greater(s.T(), currentStockStats.YearLow, 0.0)
+	require.Greater(s.T(), currentStockStats.YearHigh, 0.0)
+	require.NotEqual(s.T(), currentStockStats.WeeklyReturn, 0.0)
+	require.NotEqual(s.T(), currentStockStats.MonthlyReturn, 0.0)
+	require.NotEqual(s.T(), currentStockStats.ThreeMonthReturn, 0.0)
+	require.NotEqual(s.T(), currentStockStats.YtdReturn, 0.0)
+	require.NotEqual(s.T(), currentStockStats.YearlyReturn, 0.0)
+	require.NotEqual(s.T(), currentStockStats.LowerPriceLimit, 0.0)
+	require.NotEqual(s.T(), currentStockStats.UpperPriceLimit, 0.0)
+}
+
 func (s *FinancialFundamentalsTestSuite) TestGetTopMovers() {
 	client := newTestClient(s.Config)
 
