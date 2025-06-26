9d2fd15080846252095bf1556e7a31acc189915f
Kerem Balic <92550348+KereMBC@users.noreply.github.com>
2025-06-26 16:56:57 +0300

    #77006 feat(key insight): add key insight API

---
+++ key-insight.go
@@ -0,0 +1,31 @@
+package laplace
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+type KeyInsight struct {
+	Symbol  string `json:"symbol"`
+	Insight string `json:"insight"`
+}
+
+func (c *Client) GetKeyInsights(ctx context.Context, symbol string, region Region) (*KeyInsight, error) {
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/api/v1/key-insight", c.baseUrl), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	q := req.URL.Query()
+	q.Add("symbol", symbol)
+	q.Add("region", string(region))
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := sendRequest[KeyInsight](ctx, c, req)
+	if err != nil {
+		return nil, err
+	}
+
+	return &resp, nil
+}
