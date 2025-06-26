ebcdc510817349588261001ed24325cb3570eefd
Kerem Balic <92550348+KereMBC@users.noreply.github.com>
2025-06-26 17:03:42 +0300

    #77006 feat(stock): update tests

--- stocks_test.go
+++ stocks_test.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"testing"
 
-	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type StocksTestSuite struct {
@@ -18,15 +18,36 @@ func TestStocks(t *testing.T) {
 	})
 }
 
+func getAllAssetTypes() []AssetType {
+	return []AssetType{
+		AssetTypeStock,
+		AssetTypeForex,
+		AssetTypeIndex,
+		AssetTypeEtf,
+		AssetTypeCommodity,
+		AssetTypeStockRights,
+		AssetTypeFund,
+	}
+}
+
 func (s *StocksTestSuite) TestGetAllStocks() {
 	client := newTestClient(s.Config)
 
 	ctx := context.Background()
 
 	resp, err := client.GetAllStocks(ctx, RegionTr, 0, 0)
-	require.NoError(s.T(), err)
+	s.Require().NoError(err)
+	s.Require().NotNil(resp)
+	s.Require().Greater(len(resp), 0)
 
-	require.NotEmpty(s.T(), resp)
+	stock := resp[0]
+	s.Require().NotEqual(primitive.NilObjectID, stock.ID)
+	s.Require().NotEmpty(stock.Symbol)
+	s.Require().NotEmpty(stock.Name)
+	s.Require().Contains(getAllAssetTypes(), stock.AssetType)
+	s.Require().NotEqual(primitive.NilObjectID, stock.SectorId)
+	s.Require().NotEqual(primitive.NilObjectID, stock.IndustryId)
+	s.Require().NotEmpty(stock.UpdatedDate)
 }
 
 func (s *StocksTestSuite) TestGetAllStocksPaginated() {
@@ -34,11 +55,17 @@ func (s *StocksTestSuite) TestGetAllStocksPaginated() {
 
 	ctx := context.Background()
 
-	resp, err := client.GetAllStocks(ctx, RegionTr, 5, 10)
-	require.NoError(s.T(), err)
+	resp, err := client.GetAllStocks(ctx, RegionTr, 0, 10)
+	s.Require().NoError(err)
+	s.Require().NotNil(resp)
+	s.Require().Equal(10, len(resp))
 
-	require.NotEmpty(s.T(), resp)
-	require.Equal(s.T(), 10, len(resp))
+	for _, stock := range resp {
+		s.Require().NotEqual(primitive.NilObjectID, stock.ID)
+		s.Require().NotEmpty(stock.Symbol)
+		s.Require().NotEmpty(stock.Name)
+		s.Require().Contains(getAllAssetTypes(), stock.AssetType)
+	}
 }
 
 func (s *StocksTestSuite) TestGetStockDetailByID() {
@@ -46,10 +73,18 @@ func (s *StocksTestSuite) TestGetStockDetailByID() {
 
 	ctx := context.Background()
 
-	resp, err := client.GetStockDetailByID(ctx, "61dd0d6f0ec2114146342fd0", LocaleTr)
-	require.NoError(s.T(), err)
-
-	require.NotEmpty(s.T(), resp)
+	resp, err := client.GetStockDetailByID(ctx, "61dd0d960ec2114146343068", LocaleTr)
+	s.Require().NoError(err)
+	
+	s.Require().NotEqual(primitive.NilObjectID, resp.ID)
+	s.Require().Equal("TOASO", resp.Symbol)
+	s.Require().NotEmpty(resp.Name)
+	s.Require().Equal(string(RegionTr), resp.Region)
+	s.Require().Equal(AssetClassEquity, resp.AssetClass)
+	s.Require().Contains(string(AssetTypeStock), resp.AssetType)
+	s.Require().NotEqual(primitive.NilObjectID, resp.SectorId)
+	s.Require().NotEqual(primitive.NilObjectID, resp.IndustryId)
+	s.Require().Equal(true, resp.Active)
 }
 
 func (s *StocksTestSuite) TestGetStockDetailBySymbol() {
@@ -57,10 +92,18 @@ func (s *StocksTestSuite) TestGetStockDetailBySymbol() {
 
 	ctx := context.Background()
 
-	resp, err := client.GetStockDetailBySymbol(ctx, "TUPRS", AssetClassEquity, RegionTr, LocaleTr)
-	require.NoError(s.T(), err)
+	resp, err := client.GetStockDetailBySymbol(ctx, "TOASO", AssetClassEquity, RegionTr, LocaleTr)
+	s.Require().NoError(err)
 
-	require.NotEmpty(s.T(), resp)
+	s.Require().NotEqual(primitive.NilObjectID, resp.ID)
+	s.Require().Equal("TOASO", resp.Symbol)
+	s.Require().NotEmpty(resp.Name)
+	s.Require().Equal(string(RegionTr), resp.Region)
+	s.Require().Equal(AssetClassEquity, resp.AssetClass)
+	s.Require().Contains(string(AssetTypeStock), resp.AssetType)
+	s.Require().NotEqual(primitive.NilObjectID, resp.SectorId)
+	s.Require().NotEqual(primitive.NilObjectID, resp.IndustryId)
+	s.Require().Equal(true, resp.Active)
 }
 
 func (s *StocksTestSuite) TestGetHistoricalPrices() {
@@ -69,12 +112,21 @@ func (s *StocksTestSuite) TestGetHistoricalPrices() {
 	ctx := context.Background()
 
 	resp, err := client.GetHistoricalPrices(ctx, []string{"TUPRS", "SASA"}, RegionTr, []HistoricalPricePeriod{HistoricalPricePeriodOneDay, HistoricalPricePeriodOneWeek, HistoricalPricePeriodOneMonth})
-	require.NoError(s.T(), err)
+	s.Require().NoError(err)
+	s.Require().NotNil(resp)
+	s.Require().Greater(len(resp), 0)
 
-	require.NotEmpty(s.T(), resp)
-
-	for _, price := range resp {
-		require.NotEmpty(s.T(), price)
+	for _, priceGraph := range resp {
+		s.Require().NotEmpty(priceGraph.Symbol)
+		
+		if len(priceGraph.OneDay) > 0 {
+			point := priceGraph.OneDay[0]
+			s.Require().Greater(point.Date, int64(0))
+			s.Require().Greater(point.Close, 0.0)
+			s.Require().Greater(point.High, 0.0)
+			s.Require().Greater(point.Low, 0.0)
+			s.Require().Greater(point.Open, 0.0)
+		}
 	}
 }
 
@@ -84,21 +136,16 @@ func (s *StocksTestSuite) TestGetCustomHistoricalPrices() {
 	ctx := context.Background()
 
 	resp, err := client.GetCustomHistoricalPrices(ctx, "TUPRS", RegionTr, "2024-01-01", "2024-03-01", HistoricalPriceIntervalOneDay, false)
-	require.NoError(s.T(), err)
-
-	require.NotEmpty(s.T(), resp)
-
-	for _, price := range resp {
-		require.NotEmpty(s.T(), price)
-	}
-
-	resp, err = client.GetCustomHistoricalPrices(ctx, "TUPRS", RegionTr, "2024-01-01 10:00:00", "2024-01-02 10:00:00", HistoricalPriceIntervalOneHour, true)
-	require.NoError(s.T(), err)
-
-	require.NotEmpty(s.T(), resp)
+	s.Require().NoError(err)
+	s.Require().NotNil(resp)
+	s.Require().Greater(len(resp), 0)
 
 	for _, price := range resp {
-		require.NotEmpty(s.T(), price)
+		s.Require().Greater(price.Date, int64(0))
+		s.Require().Greater(price.Close, 0.0)
+		s.Require().Greater(price.High, 0.0)
+		s.Require().Greater(price.Low, 0.0)
+		s.Require().Greater(price.Open, 0.0)
 	}
 }
 
@@ -108,9 +155,32 @@ func (s *StocksTestSuite) TestGetStockRestrictions() {
 	ctx := context.Background()
 
 	resp, err := client.GetStockRestrictions(ctx, "TUPRS", RegionTr)
-	require.NoError(s.T(), err)
+	s.Require().NoError(err)
+	s.Require().NotNil(resp)
+	
+	for _, restriction := range resp {
+		s.Require().Greater(restriction.ID, 0)
+		s.Require().NotEmpty(restriction.Title)
+		s.Require().NotEmpty(restriction.Market)
+		s.Require().NotEmpty(restriction.StartDate)
+		s.Require().NotEmpty(restriction.EndDate)
+	}
+}
 
-	require.NotNil(s.T(), resp)
+func (s *StocksTestSuite) TestGetAllRestrictions() {
+	client := newTestClient(s.Config)
+
+	ctx := context.Background()
+
+	resp, err := client.GetAllRestrictions(ctx, RegionTr)
+	s.Require().NoError(err)
+	s.Require().NotNil(resp)
+	
+	for _, restriction := range resp {
+		s.Require().Greater(restriction.ID, 0)
+		s.Require().NotEmpty(restriction.Title)
+		s.Require().NotEmpty(restriction.Description)
+	}
 }
 
 func (s *StocksTestSuite) TestGetTickRules() {
@@ -119,7 +189,6 @@ func (s *StocksTestSuite) TestGetTickRules() {
 	ctx := context.Background()
 
 	resp, err := client.GetTickRules(ctx, "TUPRS", RegionTr)
-	require.NoError(s.T(), err)
-
-	require.NotEmpty(s.T(), resp.Rules)
+	s.Require().NoError(err)
+	s.Require().NotNil(resp)
 }
