9453cf4d40a68d27ad917a074d479cf4b1ba3e43
Edip Emre Bodur <e.bodur@papara.com>
2025-08-12 14:42:23 +0300

    #80854 feat(missing-functions): added missing client functions

---
+++ state.go
@@ -0,0 +1,38 @@
+package laplace
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+type MarketState struct {
+	ID            int       `json:"id"`
+	MarketSymbol  *string   `json:"marketSymbol,omitempty"`
+	State         string    `json:"state"`
+	LastTimestamp time.Time `json:"lastTimestamp"`
+	StockSymbol   *string   `json:"stockSymbol,omitempty"`
+}
+
+func (c *Client) GetStateOfAllMarkets(ctx context.Context, region Region, page, size int) (PaginatedResponse[*MarketState], error) {
+	endpoint := fmt.Sprintf("%s/api/v1/state/all", c.baseUrl)
+	req, err := http.NewRequest(http.MethodGet, endpoint, nil)
+	if err != nil {
+		return PaginatedResponse[*MarketState]{}, err
+	}
+
+	q := req.URL.Query()
+	q.Add("region", string(region))
+	q.Add("page", strconv.Itoa(page))
+	q.Add("size", strconv.Itoa(size))
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := sendRequest[PaginatedResponse[*MarketState]](ctx, c, req)
+	if err != nil {
+		return PaginatedResponse[*MarketState]{}, err
+	}
+
+	return resp, nil
+}
